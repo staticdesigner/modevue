61c2d4373a003f051ce441ef44900847da3f7703
staticdesigner <40516810+staticdesigner@users.noreply.github.com>
2021-07-19 08:45:54 +0000

    Deploying to gh-pages from  @ 896e77f650a1b04c18bfc591ff39754daf5da51e 🚀

--- _nuxt/static/1626684073/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fcontact","\u002Fgallery","\u002Fpricing","\u002Fservices","\u002Fabout","\u002F","\u002Fblog"]})
---
+++ _nuxt/static/1626684348/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fpricing","\u002Fservices","\u002Fblog","\u002Fcontact","\u002Fgallery"]})
